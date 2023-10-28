ccac1f931b3f8287b6af2393fc6e346aa81cc724
Javier <you@example.com>
2023-10-28 18:12:33 -0300

    Step 7 - Optimizacion de funciones

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -42,11 +42,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun initData() {
         getRetrofit()
-        getMovies()
     }
 
     private fun initResponse() {
         CoroutineScope(Dispatchers.IO).launch { getResponse() }
+        orderMovies("")
     }
 
     private fun initRecyclerView() {
@@ -58,9 +58,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initListeners() {
-        binding.btnPopularity.setOnClickListener { orderByPopularity() }
-        binding.btnOrder.setOnClickListener { orderByName() }
-        binding.btnNewest.setOnClickListener { orderByNewest() }
+        binding.btnPopularity.setOnClickListener { orderMovies("popularity") }
+        binding.btnOrder.setOnClickListener { orderMovies("title") }
+        binding.btnNewest.setOnClickListener { orderMovies("date") }
         binding.sMovie.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 searchByName(query.orEmpty())
@@ -70,10 +70,24 @@ class MainActivity : AppCompatActivity() {
             override fun onQueryTextChange(newText: String?): Boolean {
                 return false
             }
-        })
+        }) // FALTA ESTO
     }
 
-    ///--------------------------------------------///
+    private fun getRetrofit(): Retrofit {
+        retrofit = Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(OkHttpClient.Builder().addInterceptor { chain ->
+                val request = chain.request().newBuilder().addHeader("accept", "application/json")
+                    .addHeader(
+                        "Authorization",
+                        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZjRlZjZhYWVhYWI4ZjE2NDkwZDM3NzgzYzYwY2M0YSIsInN1YiI6IjY1Mzg1MDc0OWMyNGZjMDEyNjU2NzI1MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NyBfq3p6GMi2tE4HQc_jSq_UtyMIe683PPu771dcjMU"
+                    )
+                    .build()
+                chain.proceed(request)
+            }.build()).build()
+        return retrofit
+    }
 
     private suspend fun getResponse(): MoviesDataResponse? {
         val myResponse = retrofit.create(ApiService::class.java).getMoviesName()
@@ -81,47 +95,13 @@ class MainActivity : AppCompatActivity() {
         return response
     }
 
-    ///--------------------------------------------///
-
-    private fun orderByPopularity() {
+    private fun orderMovies(orderBy: String) {
         binding.progressBar.isVisible = true
         CoroutineScope(Dispatchers.IO).launch {
             val response = getResponse()
             if (response != null) {
                 runOnUiThread {
-                    adapter.updateList(response.results, "popularity")
-                    binding.progressBar.isVisible = false
-                    binding.rvMovies.adapter = adapter
-                }
-            } else {
-                Log.i("Javi", "Error al ordenar por popularidad")
-            }
-        }
-    }
-
-    private fun orderByName() {
-        binding.progressBar.isVisible = true
-        CoroutineScope(Dispatchers.IO).launch {
-            val response = getResponse()
-            if (response != null) {
-                runOnUiThread {
-                    adapter.updateList(response.results, "title")
-                    binding.progressBar.isVisible = false
-                    binding.rvMovies.adapter = adapter
-                }
-            } else {
-                Log.i("Javi", "Error al ordenar")
-            }
-        }
-    }
-
-    private fun orderByNewest() {
-        binding.progressBar.isVisible = true
-        CoroutineScope(Dispatchers.IO).launch {
-            val response = getResponse()
-            if (response != null) {
-                runOnUiThread {
-                    adapter.updateList(response.results, "date")
+                    adapter.updateList(response.results, orderBy)
                     binding.progressBar.isVisible = false
                     binding.rvMovies.adapter = adapter
                 }
@@ -149,40 +129,8 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-    }
+    } // FALTA VER ESTO
 
-    ///--------------------------------------------///
 
-    private fun getRetrofit(): Retrofit {
-        retrofit = Retrofit.Builder()
-            .baseUrl("https://api.themoviedb.org/3/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(OkHttpClient.Builder().addInterceptor { chain ->
-                val request = chain.request().newBuilder().addHeader("accept", "application/json")
-                    .addHeader(
-                        "Authorization",
-                        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZjRlZjZhYWVhYWI4ZjE2NDkwZDM3NzgzYzYwY2M0YSIsInN1YiI6IjY1Mzg1MDc0OWMyNGZjMDEyNjU2NzI1MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NyBfq3p6GMi2tE4HQc_jSq_UtyMIe683PPu771dcjMU"
-                    )
-                    .build()
-                chain.proceed(request)
-            }.build()).build()
-        return retrofit
-    }
 
-    private fun getMovies() {
-        CoroutineScope(Dispatchers.IO).launch {
-            val myResponse = retrofit.create(ApiService::class.java).getMoviesName()
-            if (myResponse.isSuccessful) {
-                val response: MoviesDataResponse? = myResponse.body()
-                if (response != null) {
-                    runOnUiThread {
-                        adapter.updateList(response.results)
-                        binding.rvMovies.adapter = adapter
-                    }
-                }
-            } else {
-                Log.i("Javi", "error")
-            }
-        }
-    }
 }
